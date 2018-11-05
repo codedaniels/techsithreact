abefe47a6874fb680ee6b18d0cb4db75b57843ac
Daniel S <danielksstone@gmail.com>
2018-11-05 09:51:19 -0800

    updating to states and props vid specs

--- src/users/Users.js
+++ src/users/Users.js
@@ -5,10 +5,7 @@ class Users extends Component {
     render(){
         return (
             <div>
-                <h1>{this.props.title}</h1>
-                <User>John</User>
-                <User age="30"></User>
-                <User age="40">Jingleheimerschmit</User>
+                <User/>
             </div>
         )
     }
