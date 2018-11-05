14ff0f631d50e7a3924e43322370cda150a7f33f
Daniel S <danielksstone@gmail.com>
2018-11-05 11:11:55 -0800

    dynamic

--- src/users/Users.js
+++ src/users/Users.js
@@ -2,10 +2,24 @@ import React, {Component} from 'react';
 import User from './User';
 
 class Users extends Component {
+    state = {
+        users: [
+            {name:"John", age:20},
+            {name:"Jacob", age:30},
+            {name:"Jingleheimerschmit", age:40},
+        ],
+        title:"Users List"
+    }
+
     render(){
         return (
             <div>
-                <User/>
+                <button>Make Us 10 Years Younger</button>
+                <br/>
+                <h1>{this.state.title}</h1>
+                <User age={this.state.users[0].age}>{this.state.users[0].name}</User>
+                <User age="30">Jacob</User>
+                <User age="40">Jingleheimerschmit</User>
             </div>
         )
     }
